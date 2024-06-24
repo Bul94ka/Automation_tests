566852463a49b8daa5f8828f569bc1e32c6770a9
Vladyslav Hresko <vgresko.vg@gmail.com>
2024-06-24 14:45:32 +0200

    added homework

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,24 +1,8 @@
-/*First autotest Test
-
+//Test 1 (Homework)
 describe("Our first test suite", () => {
     it("Test visits site LMS", () => {
         //cypress code
         cy.visit("https://www.edu.goit.global/account/login");
-
-        /*TEST CASES 1
-
-        //Find input with email address and paste email"
-        cy.get("#user_email").type("user888@gamil.com");
-
-        //Find password with password address and paste password"
-        cy.get("#user_password").type("1234567890");
-
-        //Click on a login button
-        cy.get(".eckniwg2").click();
-        cy.get(".eckniwg2").should('have.class',"active");
-        
-        
-        TESTS CASES 2 
 
         //Find input with email address and paste email"
         cy.get("#user_email").type("user888@gmail.com");
@@ -27,14 +11,53 @@ describe("Our first test suite", () => {
         cy.get("#user_password").type("1234567890");
 
         //Click on a login button
-        cy.get("[type= 'submit']").click();
+        cy.get(".eckniwg2").click();
 
-        //Scroll into view
+        //Verify that the login was successful by checking for a specific element that appears after login
+        cy.get('#open-navigation-menu-mobile').should("be.visible");
+
+        //Open the navigation menu
+        cy.get('#open-navigation-menu-mobile').click();
+
+        //Find the log out button in the menu and click it
         cy.get('[id="go-to-the-course-homepage-widget"]').scrollIntoView().should("be.visible");
+        cy.get(':nth-child(12) > .next-bve2vl').click();
+        
+        //Verify that the user is redirected to the login page
+        cy.url().should('include', '/account/login');
         
     });
 });
-*/
+
+//Test 2 (Homework)
+describe("Our second test suite", () => {
+    it("Test visits site LMS and logs in with new credentials", () => {
+
+        cy.visit("https://www.edu.goit.global/account/login");
+        //Find input with email address and paste email"
+        cy.get("#user_email").type("testowyqa@qa.team");
+
+        //Find password with password address and paste password"
+        cy.get("#user_password").type("QA!automation-1");
+
+        //Click on a login button
+        cy.get(".eckniwg2").click();
+
+        //Verify that the login was successful by checking for a specific element that appears after login
+        cy.get('#open-navigation-menu-mobile').should("be.visible");
+
+        //Open the navigation menu
+        cy.get('#open-navigation-menu-mobile').click();
+
+        //Find the log out button in the menu and click it
+        cy.get('[id="go-to-the-course-homepage-widget"]').scrollIntoView().should("be.visible");
+        cy.get(':nth-child(8) > .next-bve2vl').click();
+        
+        //Verify that the user is redirected to the login page
+        cy.url().should('include', '/account/login');
+    });
+});
+
 
 /*Second autotest for login user, admin, manager
 describe("login test", () => {
@@ -64,7 +87,7 @@ describe("login test", () => {
 });
 */
 
-//Third autotest for API
+/*Third autotest for API
 describe("httpbin tests", () => {
     const request = {
         url: 'https://httpbin.org/non-existing-url',
@@ -81,3 +104,4 @@ describe("httpbin tests", () => {
     });
 
 });
+*/
